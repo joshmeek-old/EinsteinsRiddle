91075c03ff9f8652c123698dcbf83108d15cf699
Josh Meek <joshmeek18@gmail.com>
2015-02-07 01:35:43 -0500

    Initial commit

---
+++ src/main/java/com/joshmeek/einsteinsriddle/MainRunner.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.joshmeek.einsteinsriddle;
+
+/**
+ *
+ * @author josh
+ */
+public class MainRunner {
+    
+    public static void main(String[] args) {
+        
+        GUI gui = new GUI();
+        gui.setLocationRelativeTo(null);
+        gui.setVisible(true);
+        
+    }
+    
+}
